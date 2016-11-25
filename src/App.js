35be42dccca56e95d9924b455606e56b42262e24
Dohxis <dohxis@gmail.com>
2016-11-26 01:33:20 +0200

    Restores login

--- src/App.js
+++ src/App.js
@@ -10,6 +10,16 @@ import Test from './pages/Test/Test'
 import store from './store'
 
 import './Global.css'
+import * as firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyA_9CkxUEVjmJB34iVLg8sq-3UFWKaRKdU",
+    authDomain: "denoodle-3c592.firebaseapp.com",
+    databaseURL: "https://denoodle-3c592.firebaseio.com",
+    storageBucket: "denoodle-3c592.appspot.com",
+    messagingSenderId: "54608870615"
+};
+firebase.initializeApp(config);
 
 var auth = new Auth0Lock('5lEbkHrNuPFVqEKoE5M9LcjMy40ucITe', 'denoodle.eu.auth0.com', {
     languageDictionary: {
@@ -26,13 +36,22 @@ var auth = new Auth0Lock('5lEbkHrNuPFVqEKoE5M9LcjMy40ucITe', 'denoodle.eu.auth0.
     }
 });
 
+function restore_login(){
+    if(!store.user.name && localStorage.getItem('id_auth')) {
+        const user = firebase.database().ref().child('users/' + localStorage.getItem('id_auth'));
+        user.on('value', snap => {
+            store.login(snap.val().identity, localStorage.getItem('id_auth'));
+        });
+    }
+}
+
 class App extends Component {
   render() {
     return (
         <Router history={ browserHistory }>
             <Route path="app" component={ Master }>
-                <IndexRoute component={ Test } auth={auth} store={store} />
-                <Route path="test" component={ Test } auth={auth} store={store} />
+                <IndexRoute component={ Test } auth={auth} store={store} onEnter={restore_login}/>
+                <Route path="test" component={ Test } auth={auth} store={store} onEnter={restore_login}/>
             </Route>
             <Route path="/" component={ Landing } auth={auth} store={store} />
         </Router>
