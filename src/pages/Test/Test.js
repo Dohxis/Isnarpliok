15df5e04ee876a414225b6005fbec504c2500db4
vodpox <edvinas.senda@gmail.com>
2016-11-25 23:05:47 +0200

    some functionality for eval

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {observer} from 'mobx-react'
 import * as firebase from 'firebase'
 import {browserHistory} from 'react-router'
+import { Button } from 'semantic-ui-react'
 
 import { Grid, Image, TextArea } from 'semantic-ui-react'
 import './style.css';
@@ -18,29 +19,46 @@ import Tab from 'muicss/lib/react/tab';
 
 @observer
 class Test extends Component {
-
-	componentWillMount(){
-        const user = firebase.database().ref().child('users/' + localStorage.getItem('id_auth') + '/active');
-        user.on('value', snap => {
-            if(!snap.val())
-                browserHistory.push('/app/select');
-        });
+	
+	constructor() {
+		super();
+		this.state = { code: '' };
 	}
 	
-	onChange(i, value, tab, ev) {
-		console.log(arguments);
+	componentWillMount(){
+    const user = firebase.database().ref().child('users/' + localStorage.getItem('id_auth') + '/active');
+    user.on('value', snap => {
+      if(!snap.val())
+      	browserHistory.push('/app/select');
+    });
+	}
+	
+	onTabChange(i, value, tab, ev) {
+		//console.log(arguments);
 	}
 
-	onActive(tab) {
-		console.log(arguments);
+	onTabActive(tab) {
+		//console.log(arguments);
+	}
+	
+	onCodeChange(newValue) {
+		this.state.code = newValue;
+	}
+	
+	onRun() {
+		var codeToEval = this.state.code;
+		
+		codeToEval = codeToEval.replace(/console.log/g, "printToTerminal");
+		
+		console.log(codeToEval);
+		eval(codeToEval);
+	}
+	
+	onSubmit() {
+		alert('TODO: check if task is done');
 	}
 	
 	render(){
-
-		
-		function onChange(newValue) {
-		  console.log('change',newValue);
-		}
 
 		return (
 			<div>
@@ -52,15 +70,15 @@ class Test extends Component {
 								height="calc(100vh - 70px)"
 								mode="javascript"
 								theme="github"
-								onChange={onChange}
+								onChange={this.onCodeChange.bind(this)}
 								name="46512546"
 								editorProps={{$blockScrolling: true}}
 								className="code-editor"
-								/>
+							/>
 						</Grid.Column>
 						<Grid.Column>
-							<Tabs onChange={this.onChange} initialSelectedIndex={0} justified>
-				        <Tab value="pane-1" label="Task" onActive={this.onActive}>
+							<Tabs onChange={this.onTabChange} initialSelectedIndex={0} justified>
+				        <Tab value="pane-1" label="Task" onActive={this.onTabActive}>
 									<p>
 										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ante. Mauris eleifend, quam a vulputate dictum, massa quam dapibus leo, eget vulputate orci purus ut lorem. In fringilla mi in ligula. Pellentesque aliquam quam vel dolor. Nunc adipiscing. Sed quam odio, tempus ac, aliquam molestie, varius ac, tellus. Vestibulum ut nulla aliquam risus rutrum interdum. Pellentesque lorem. Curabitur sit amet erat quis risus feugiat viverra. Pellentesque augue justo, sagittis et, lacinia at, venenatis non, arcu. Nunc nec libero. In cursus dictum risus. Etiam tristique nisl a nulla. Ut a orci. Curabitur dolor nunc, egestas at, accumsan at, malesuada nec, magna.
 									</p>
@@ -79,6 +97,16 @@ class Test extends Component {
 									<p>
 										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ante. Mauris eleifend, quam a vulputate dictum, massa quam dapibus leo, eget vulputate orci purus ut lorem. In fringilla mi in ligula. Pellentesque aliquam quam vel dolor. Nunc adipiscing. Sed quam odio, tempus ac, aliquam molestie, varius ac, tellus. Vestibulum ut nulla aliquam risus rutrum interdum. Pellentesque lorem. Curabitur sit amet erat quis risus feugiat viverra. Pellentesque augue justo, sagittis et, lacinia at, venenatis non, arcu. Nunc nec libero. In cursus dictum risus. Etiam tristique nisl a nulla. Ut a orci. Curabitur dolor nunc, egestas at, accumsan at, malesuada nec, magna.
 									</p>
+									<Grid columns={2}>
+										<Grid.Row className="ide-grid-row0">
+											<Grid.Column>
+												<Button fluid color='green' onClick={this.onRun.bind(this)}>Run</Button>
+											</Grid.Column>
+											<Grid.Column>
+												<Button fluid color='yellow' onClick={this.onSubmit.bind(this)}>Submit</Button>
+											</Grid.Column>
+										</Grid.Row>
+									</Grid>
 								</Tab>
 				        <Tab value="pane-2" label="Terminal">
 									<p>
