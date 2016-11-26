fd0587eefc91f401f653b1ead1b6cf3dc0e04cfd
Dohxis <dohxis@gmail.com>
2016-11-26 05:54:08 +0200

    Test redirect

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import {observer} from 'mobx-react'
+import * as firebase from 'firebase'
+import {browserHistory} from 'react-router'
 
 @observer
 class Test extends Component {
 
 	componentWillMount(){
-		if(this.props.route.store.user.active){
-			console.log("hey");
-		} else {
-			console.log("lol");
-		}
+        const user = firebase.database().ref().child('users/' + localStorage.getItem('id_auth') + '/active');
+        user.on('value', snap => {
+            if(!snap.val())
+                browserHistory.push('/app/select');
+        });
 	}
 
 	render(){
