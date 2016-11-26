159108eae678fceb7b3587261e48ef43c8f13e0b
Lukas <theluksys3@gmail.com>
2016-11-26 05:53:04 +0200

    Landing page

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {observer} from 'mobx-react'
+import { Grid, Image } from 'semantic-ui-react';
 
 @observer
 class Test extends Component {
@@ -11,7 +12,16 @@ class Test extends Component {
 	render(){
 		return (
 			<div>
-				IDE
+				<Grid columns={2}>
+					<Grid.Row>
+						<Grid.Column>
+							;kjshdkjfhskfjshf
+						</Grid.Column>
+						<Grid.Column>
+							asdasdasdasd
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			</div>
 		);
 	}
