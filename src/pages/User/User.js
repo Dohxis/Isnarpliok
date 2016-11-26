c604b69d3fd4f80a7c8dcd9387444efd5fd67f26
Lukas <theluksys3@gmail.com>
2016-11-26 13:46:31 +0200

    User page update

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Image, Card, Progress, Grid, Table, Header, Rating, Menu, Segment} from 'semantic-ui-react'
+import { Image, Card, Progress, Grid, Table, Header, Rating, Menu, Segment, Divider } from 'semantic-ui-react'
 import * as firebase from 'firebase';
 import "./User.css";
 
@@ -54,38 +54,49 @@ class User extends Component {
 		var _this = this;
 		return (
 			<div className="App">
-				<div>
-					<Card fluid>
-				      <Card.Content className="card-prefix">
-						  <Grid columns={2}>
-							    <Grid.Row>
-							      <Grid.Column color="purple" width={2}>
-									  <center>
-										  <Image size='small' src={_this.state.user_data.identity? _this.state.user_data.identity.picture:"/logo.png"} />
-									  </center>
-							      </Grid.Column>
-							      <Grid.Column width={13}>
-									  <b> <div className="user-name"> {_this.state.user_data.identity? _this.state.user_data.identity.nickname: "spaghetti"} </div> </b>
-									  <Progress color="purple" percent={30} active>
-									    	Level 7
-									  </Progress>
-									<span>
-										<span className="extra-attributes user-since"> <b> User since: </b> 2016-13-84 </span>
-										<span className="extra-attributes completed-attribute"> <b> Completed: </b> 9001+ </span>
-									</span>
-							      </Grid.Column>
-							    </Grid.Row>
-							</Grid>
-				      </Card.Content>
-				    </Card>
+				<Card fluid className="user_fluid">
+			      <Card.Content className="card-prefix">
+					  	<Grid columns={2}>
+						    <Grid.Row>
+						        <Grid.Column width={2}>
+								    <center>
+								     	<Image className="user_image" size='small' src={_this.state.user_data.identity? _this.state.user_data.identity.picture:"/logo.png"} />
+								    </center>
+						        </Grid.Column>
+								<Grid.Column width={7}>
+									<div className="user-name"><b> {_this.state.user_data.identity? _this.state.user_data.identity.nickname: "spaghetti"}</b></div>
+									<div className="extra-attributes user-level"> <b> Level: </b> 3</div>
+									<div className="extra-attributes user-since"> <b> User since: </b> 2016-13-84 </div>
+									<div className="extra-attributes completed-attribute"> <b> Completed: </b> 9001+ </div>
+								</Grid.Column>
+								<Grid.Column width={6}>
+									
+								</Grid.Column>
+						    </Grid.Row>
+						</Grid>
+						<Divider />
+					  	<Grid columns={3} className="user_languages">
+						    <Grid.Row>
+						        <Grid.Column width={5}>
+						        	<center><Image size="small" src="/js.png" /></center>
+						        	<h2>JavaScript</h2>
+						        	<h3>6/20</h3>
+						        </Grid.Column>
+								<Grid.Column width={5}>
+						        	<center><Image size="small" src="/php.png" /></center>
+						        	<h2>PHP</h2>
+						        	<h3>Not started</h3>
+								</Grid.Column>
+						        <Grid.Column width={5}>
+						        	<center><Image size="small" src="/python.png" /></center>
+						        	<h2>Python</h2>
+						        	<h3>Not started</h3>
+						        </Grid.Column>
+						    </Grid.Row>
+						</Grid>
+			      </Card.Content>
+			    </Card>
 
-					<Menu pointing>
-				        <Menu.Item className="history-prefix" name='history' active={true}/>
-			        </Menu>
-					<Segment>
-						{_this.getHistory()}
-					</Segment>
-				</div>
 			</div>
 		)
 	}
@@ -93,3 +104,13 @@ class User extends Component {
 }
 
 export default User;
+
+/*
+
+				<Menu pointing>
+			        <Menu.Item className="history-prefix" name='history' active={true}/>
+		        </Menu>
+				<Segment>
+					{_this.getHistory()}
+				</Segment>
+*/
